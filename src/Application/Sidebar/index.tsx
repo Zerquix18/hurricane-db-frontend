3978373a01d7cdeafc418cab7528de28bd60df92
I'm Luis! <malbertoa_11@hotmail.com>
2020-01-24 01:48:12 -0400

    Add _very_ basic sidenav component

---
+++ src/Application/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <div>I am the sidebar :3</div>
+  );
+};
+
+export default Sidebar;
--- src/Application/index.tsx
+++ src/Application/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 const Application: React.FC = () => {
   return (
     <div>
       <Navbar />
-      
+      <div style={{ marginLeft: 10, marginRight: 10 }}>
+        <Grid>
+          <Grid.Column computer={3}>
+            <Sidebar />
+          </Grid.Column>
+          <Grid.Column computer={12}>
+            COLUMN 2
+          </Grid.Column>
+        </Grid>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Application;
