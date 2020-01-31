0447177e505e0c0072fa99a8e695fc9d82cb2052
I'm Luis! <malbertoa_11@hotmail.com>
2020-01-31 19:07:56 -0400

    Change default center

--- src/Application/MapArea/index.tsx
+++ src/Application/MapArea/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Map as GoogleMap } from 'UI'
 
+// The Atlantic, for now.
+const defaultCenter = {
+  lat: 21.902656006558043,
+  lng: -72.88469006709305,
+};
+
+const defaultZoom = 5;
+
 const MapArea: React.FC = () => {
   return (
-    <GoogleMap />
+    <GoogleMap
+      center={defaultCenter}
+      zoom={defaultZoom}
+    />
   );
 };
 
