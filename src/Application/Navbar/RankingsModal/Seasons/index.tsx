8303c10ba2bb95001a8ec33693905c42a0dda91f
I'm Luis! <malbertoa_11@hotmail.com>
2020-04-26 00:53:36 -0400

    add seasons

---
+++ src/Application/Navbar/RankingsModal/Seasons/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { fetchRanking } from 'api';
+import { Loader, Table } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+interface SeasonsProps {
+
+}
+
+interface Season {
+  season: number;
+  total: number;
+}
+
+const Seasons: React.FC<SeasonsProps> = () => {
+  const [seasons, setSeasons] = useState<Season[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const getSeasons = async () => {
+    try {
+      const seasons = await fetchRanking('top_by_season');
+      setSeasons(seasons);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getSeasons();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    (
+      <Table>
+        <Table.Header>
+          <Table.HeaderCell>Season</Table.HeaderCell>
+          <Table.HeaderCell>Total systems</Table.HeaderCell>
+        </Table.Header>
+        <Table.Body>
+          { seasons.map(season => {
+            const to = `/atlantic/$${season.season}`;
+            return (
+              <Table.Row key={season.season}>
+                <Table.Cell><Link to={to}>{ season.season }</Link></Table.Cell>
+                <Table.Cell>{ season.total }</Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    )
+  );
+}
+
+export default Seasons;
